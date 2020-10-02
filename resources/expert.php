f841f6e5c50b33280e4b497aca6621c1a843c4b2
MZ <nikita.zholkovskyi@gmail.com>
2020-10-02 09:31:58 +0200

    second exercise

--- resources/expert.php
+++ resources/expert.php
@@ -11,7 +11,13 @@ function new_exercise($x) {
     $block = "<br/><hr/><br/><br/>Exercise $x starts here:<br/>";
     echo $block;
 }
-new_exercise(1);
+new_exercise(2);
+$week = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+$monday = $week[0];
+
+echo $monday;
+
+
 
 
 
