90d14358a74dd163ae288c675756353e3d3143ff
MZ <nikita.zholkovskyi@gmail.com>
2020-10-02 09:28:37 +0200

    first exercise

--- resources/expert.php
+++ resources/expert.php
@@ -1,6 +1,17 @@
 <?php
-declare(strict_types=1);
 
+declare(strict_types=1);
+ini_set('display_errors', "1");
+ini_set('display_startup_errors', "1");
+error_reporting(E_ALL);
+
+echo "Exercise 1 starts here:";
+
+function new_exercise($x) {
+    $block = "<br/><hr/><br/><br/>Exercise $x starts here:<br/>";
+    echo $block;
+}
+new_exercise(1);
 
 
 
